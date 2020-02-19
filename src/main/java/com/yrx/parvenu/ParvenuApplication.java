fe86081e4fe4a2f478b643cab7f81539c47d6408
xin <18825188512@163.com>
2020-02-19 12:02:48 +0800

    init

---
+++ src/main/java/com/yrx/parvenu/ParvenuApplication.java
@@ -0,0 +1,13 @@
+package com.yrx.parvenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParvenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParvenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yrx/parvenu/ParvenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrx.parvenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParvenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
